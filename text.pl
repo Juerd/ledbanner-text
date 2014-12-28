4cbaefd720fac4f785b05363f2bf6caac86e0683
Andreas 'count' Kotes <count@flatline.de>
2014-12-28 17:07:13 +0100

    some untested evil hackery

--- text.pl
+++ text.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 use Time::HiRes qw(sleep);
+use Image::Magick;
 
 undef $/;
 $| = 1;
@@ -8,21 +9,46 @@ $| = 1;
 sub read_image {
     my ($color, $text) = @_;
 
-    open my $pipe, '-|', 'convert',
-        '-gravity' => 'center',
-        '-size' => 'x8',
-        '-font' => 'nokiafc22.ttf',
-        '-background' => 'black',
-        '-pointsize' => 8,
-        '-fill' => $color,
-        '-depth' => 8,
-        '-kerning' => 0,
-        '-interword-spacing' => 5,
-#        '-resize' => 'x8!',
-        "label:\Q$text",
-        'RGB:-'
-        or warn $!;
-    return readline $pipe;
+    # save image to string
+    my $rgb_string = "";
+    open IMAGE, '>', \$rgb_string;
+
+    # create new image structure
+    my $image = Image::Magick->new;
+    #$image->Set(debug => 'All');
+
+    # create empty image & query width
+    $image->ReadImage('canvas:black');
+    my ($x_ppem, $y_ppem, $ascender, $descender, $width, $height, $max_advance) = $image->QueryFontMetrics(
+        gravity => 'center',
+        font => 'nokiafc22.ttf',
+        background => 'black',
+        pointsize => 8,
+        fill => $color,
+        kerning => 0,
+        'interword-spacing' => 5,
+        text => $text);
+
+    # create new empty image with width
+    $image->Set(size => $width . 'x8', depth => 8);
+    $image->ReadImage('canvas:black');
+
+    # write text
+    $image->Annotate(
+        gravity => 'center',
+        font => 'nokiafc22.ttf',
+        background => 'black',
+        pointsize => 8,
+        fill => $color,
+        kerning => 0,
+        'interword-spacing' => 5,
+        text => $text);
+
+    # write image
+    $image->Write(file=>\*IMAGE, filename=>'RGB:-');
+    close IMAGE;
+
+    return $rgb_string;
 }
 
 my $color = shift @ARGV;
